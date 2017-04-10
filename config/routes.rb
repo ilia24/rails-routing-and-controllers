0f180d388ed821910f07998e0f696d1f920c6a46
ilia24 <iliademertchian24@gmail.com>
2017-04-10 17:30:34 -0400

    Added route prefixes'

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/' => 'pages#welcome'
+  root 'pages#welcome'
+
   get '/welcome' => 'pages#welcome'
 
   get '/about' => 'pages#about'
 
   get '/contest' => 'pages#contest'
 
-  get '/kitten/:size' => 'pages#kitten'
-
-  get '/kittens/:size' => 'pages#kittens'
+  get '/kitten/:size' => 'pages#kitten', as: 'kitten'
+  get '/kittens/:size' => 'pages#kittens', as: 'kittens'
 
   get '/secrets/:magic_word' => 'pages#passcheck'
   get '/secrets' => 'pages#secrets'
